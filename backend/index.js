c796b02e43fe785929db0fb0a511b87edcb90795
osmanpehlivanoglu <ospehlivano@gmail.com>
2023-11-30 22:34:42 +0300

    get ve getbyid

--- backend/index.js
+++ backend/index.js
@@ -13,19 +13,71 @@ const Book = require('./models/Book')
 // 
 
 app.get('/', (req, res) => {
-    res.status(201).send("hello world")
+    try {
+
+        res.status(201).send("hello world")
+
+    } catch (error) {
+        return res.status(500).send({ message: error.message })
+    }
 })
+
 
 
 app.post('/books', async (req, res) => {
 
-    console.log(req.body);
+    try {
 
-    const newBook = { title: req.body.title, author: req.body.author }
-    console.log(newBook);
+        if (!req.body.title || !req.body.author) {
+            return res.status(404).send({ message: "Hata! title ve author gerekli" })
+        }
 
-    const book = await Book.create(newBook)
-    return res.status(201).send({ message: "Başarılı", data: book })
+        const newBook = { title: req.body.title, author: req.body.author, publishYear: req.body.publishYear }
+        const book = await Book.create(newBook)
+        return res.status(201).send({ message: "Başarılı", data: book })
+
+    } catch (error) {
+
+        return res.status(500).send({ message: error.message })
+
+    }
+
+})
+
+app.get('/books', async (req, res) => {
+
+    try {
+
+        const books = await Book.find({});
+        return res.status(201).send({ message: "Başarılı", data: books, count: books.length })
+
+    } catch (error) {
+
+        return res.status(500).send({ message: error.message })
+
+    }
+
+})
+
+app.get('/books/:id', async (req, res) => {
+
+    const id = req.params.id
+    // const {id, elma} = req.params
+
+    console.log(id);
+
+
+    try {
+
+        const book = await Book.findById(id)
+        return res.status(201).send({ message: "Başarılı", data: book })
+
+
+    } catch (error) {
+
+        return res.status(500).send({ message: error.message })
+
+    }
 
 })
 
--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,24 +1,21 @@
 const mongoose = require('mongoose');
 
-const bookSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-        },
-        author: {
-            type: String,
-            required: true,
-        },
-        publishYear: {
-            type: Number
-        }
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
     },
-    {
-        timestamps: true
+    author: {
+        type: String,
+        required: true,
+    },
+    publishYear: {
+        type: Number
     }
-)
+}, {
+    timestamps: true
+});
 
+module.exports = mongoose.model('Book', bookSchema);
 
-module.exports = mongoose.model('Book', bookSchema)
 
