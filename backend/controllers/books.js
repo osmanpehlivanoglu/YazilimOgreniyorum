cf39acd5f0de5d97288dc551f395c4616c11abb8
osmanpehlivanoglu <ospehlivano@gmail.com>
2023-12-29 12:43:00 +0300

    regex

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,21 +1,43 @@
 const Book = require('../models/Book')
 
 const getAllBooks = async (req, res) => {
-    //ÖDEV EKSTRALAR
-    //barkod, yayınevi, stok, fiyat  ---required
-    //ekstra kısıtlar --istediğiniz özelliklere ekleyin --max karakter vs.
+
+    console.log(req.query);
+
+    // barcode, title, author, publishYear, stock, price, isPublish
+
+
+    // kullanıcın querylerini değişkenlerimize atama
+    const { author, title, publishYear } = req.query
+    const queryObjem = {}
+
+    if (author) {
+        queryObjem.author = author;
+        // queryObjem.selamlama = "naber?"
+    }
+
+    if (title) {
+        //türkçe karakterlerde sıkıntı yaşamayalım diye
+        let modifiedTitle = title.toLocaleLowerCase().replace(/i/gi, '[iİ]').replace(/ı/gi, '[ıI]');
+        //kelime kelime aratabilmek için regex kullandık
+        queryObjem.title = { $regex: modifiedTitle, $options: 'i' };
+    }
+
+    console.log(queryObjem);
+
+    const books = await Book.find(queryObjem)
 
 
     //skip - atla demek
     //limit - şu kadar göster demek
     //query ile page ve limit alıp skip bunlarla hesaplatılıyor
-    const page = Number(req.query.page) || 1
+    /* const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page - 1) * limit
 
     console.log({ sayfa: page, limit: limit, atla: skip });
 
-    const books = await Book.find({}).skip(skip).limit(limit);
+    const books = await Book.find({}).skip(skip).limit(limit); */
     return res.status(201).send({ message: "Başarılı", data: books, count: books.length })
 
 }
@@ -35,7 +57,15 @@ const getBook = async (req, res) => {
 
 const addBook = async (req, res) => {
 
-    const newBook = { title: req.body.title, author: req.body.author, publishYear: req.body.publishYear }
+    const newBook = {
+        title: req.body.title,
+        author: req.body.author,
+        publishYear: req.body.publishYear,
+        barcode: req.body.barcode,
+        stock: req.body.stock,
+        price: req.body.price,
+        isPublish: req.body.isPublish,
+    }
     const book = await Book.create(newBook)
     return res.status(201).send({ message: "Başarılı", data: book })
 
--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
+    barcode: {
+        type: String,
+        required: [true, "Barkodu boş bırakmayınız."],
+        unique: true,
+    },
     title: {
         type: String,
         required: [true, "isim boş bırakılamaz"],
@@ -11,10 +16,28 @@ const bookSchema = new mongoose.Schema({
     },
     publishYear: {
         type: Number
+    },
+    stock: {
+        type: Number,
+        required: [true, "stok miktarı boş bırakılamaz"],
+        min: [0, "stok miktarı en az 0 olabilir"],
+    },
+    price: {
+        type: Number,
+        required: [true, "fiyat boş bırakılamaz"],
+        min: [0, "fiyat en az 0 TL olabilir"],
+    },
+    isPublish: {
+        type: Boolean,
+        default: true,
     }
+
 }, {
     timestamps: true
 });
+
+
+
 
 module.exports = mongoose.model('Book', bookSchema);
 
